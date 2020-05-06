4710ac55dcbd70d70e35db514fab346cda3709f1
bocar <bocarsock5@gmail.com>
2020-05-06 18:23:19 -0400

    js file with code to prepend text to empty html div

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//alert('loaded');
+jQuery(document).ready(function($) {
+    $('#task').on('submit',function(e) {
+        e.preventDefault();
+        var preprend = $('#text').val();
+        if(preprend!=''){
+            console.log(preprend);
+            var str1="<p>";
+            var str2="</p>";
+            var res = str1.concat(preprend, str2);
+            $( "#tasks" ).prepend( res );
+        }else{
+            alert('Text empty!');
+        }
+    })
+})
